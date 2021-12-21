da338aa0934f84c2e8081584dc2206406a9b4e28
stan <stan.luo@iag.com.au>
2021-12-21 11:37:26 +1100

    Basic home screen + paper provider

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Button} from 'react-native-paper';
+import styled from 'styled-components';
 
 export function HomeScreen() {
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <HomeContainer>
+      <HomeButton mode="outlined" onPress={() => console.log('Pressed')}>
+        Map View
+      </HomeButton>
+      <HomeButton mode="outlined" onPress={() => console.log('Pressed')}>
+        Scan
+      </HomeButton>
+    </HomeContainer>
   );
 }
+
+const HomeContainer = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HomeButton = styled(Button)`
+  margin: 16px;
+`;
