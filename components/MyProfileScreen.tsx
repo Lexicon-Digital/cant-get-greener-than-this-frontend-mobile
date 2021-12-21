d1af29147c5bda9d7d724c334917f9cf5ef535c3
stan <stan.luo@iag.com.au>
2021-12-21 15:28:14 +1100

    Dashboard WIP

--- components/MyProfileScreen.tsx
+++ components/MyProfileScreen.tsx
@@ -1,18 +1,58 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Button} from 'react-native-paper';
+import {
+  Button,
+  Headline,
+  Subheading,
+  List,
+  Card,
+  Title,
+  Paragraph,
+} from 'react-native-paper';
 import styled from 'styled-components';
+import {SAVE_RECORDS} from '../constants/profile';
 
 export function MyProfileScreen({navigation}: {navigation: any}) {
-  return <HomeContainer></HomeContainer>;
+  return (
+    <HomeContainer>
+      <Headline>Thank you!</Headline>
+      <Subheading>
+        You have saved the earth 0.32 metric tons of CO2 and earned $134.7
+      </Subheading>
+
+      <SavesContainer>
+        <Card.Content>
+          <Title>Earned Gas Discounts</Title>
+          {SAVE_RECORDS.map(({amount, date}) => (
+            <List.Item
+              title={`Saved $${amount}`}
+              description={date}
+              left={props => (
+                <IconContainer>
+                  <Paragraph>🌿</Paragraph>
+                </IconContainer>
+              )}
+            />
+          ))}
+        </Card.Content>
+      </SavesContainer>
+    </HomeContainer>
+  );
 }
 
 const HomeContainer = styled(View)`
   flex: 1;
+  padding: 24px;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const IconContainer = styled(View)`
   justify-content: center;
   align-items: center;
 `;
 
-const HomeButton = styled(Button)`
-  margin: 16px;
+const SavesContainer = styled(Card)`
+  flex: 1;
+  margin-top: 24px;
 `;
