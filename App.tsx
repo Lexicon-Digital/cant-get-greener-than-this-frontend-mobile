808bd0ad2f3499f66b0caff97179200fe5dce77a
Kenny Goh <Kenny.Goh@iag.com.au>
2021-12-21 11:25:28 +1100

    added sign in and sign up placeholders

--- App.tsx
+++ App.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from './components/HomeScreen';
+import {SignInScreen} from './components/SignInScreen';
+import {SignUpScreen} from './components/SignUpScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const isSignedIn = false;
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator>
+        {isSignedIn ? (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          <>
+            <Stack.Screen name="Sign In" component={SignInScreen} />
+            <Stack.Screen name="Sign Up" component={SignUpScreen} />
+          </>
+        )}
         {/*<Stack.Screen name="Details" component={DetailsScreen} />*/}
       </Stack.Navigator>
     </NavigationContainer>
---
+++ components/SignInScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export function SignInScreen() {
+  return (
+    <View>
+      <Text>Sign in Screen placeholder</Text>
+    </View>
+  );
+}
