c000d5788d5c5a74f0ab681b2a352c7096c62283
Kenny Goh <Kenny.Goh@iag.com.au>
2021-12-21 11:57:36 +1100

    some preliminary progress on login

--- components/SignInScreen.tsx
+++ components/SignInScreen.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {Button, Divider} from 'react-native-paper';
+import styled from 'styled-components';
 
 // @ts-ignore
 export function SignInScreen({navigation}) {
   return (
-    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+    <SignInContainer>
       {/* todo: nice app image here  */}
       <Button>Register me</Button>
       <Divider />
       <Text>Already a member?</Text>
       <Button onPress={() => navigation.navigate('Login')}>Login</Button>
-    </View>
+    </SignInContainer>
   );
 }
+
+const SignInContainer = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
